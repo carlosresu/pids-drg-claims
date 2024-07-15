4d2b834fa3ed75967777f0a0956a0d9dacf94c18
Carlos Resurreccion <resurreccion.cmc@gmail.com>
2024-07-15 13:58:32 +0800

    add file-paths.R and main-functions.R to clean up main script for readability

--- data-cleaning/data-formats.R
+++ data-cleaning/data-formats.R
@@ -1,3 +1,10 @@
+na_values <- c('NONE', 'None', '-', '--', '---', 'N/A', 'n/a', 'nan', 'NAN')
+na_like_strings <- c("", " ", "  ", "-", "none", "None", "NONE", "NA", "n/a", 
+                     "N/A", "NaN", "'", "\t", "\n", "\r", "\f", "\v", "\u00A0",
+                     "\u2000", "\u2001", "\u2002", "\u2003", "\u2004", "\u2005", 
+                     "\u2006", "\u2007", "\u2008", "\u2009", "\u200A", "\u2028", 
+                     "\u2029", "\u202F", "\u205F", "\u3000")
+
 # Define column types
 character_cols <- c("PSEUDO_CLAIMSERIES", "PSEUDO_MEM_PIN", "HCI_PMCC_NO", "HCP_NO_LIST", "PRIMARY_ILLNESS", 
                     "SECONDARY_ILLNESS", paste0("ICDCODE", c(1:14, 16:170)), "ICCODED15", paste0("RVSCODE", 1:20), 
