355fa845ba992a014cf5dacd7760283448d44a0f
Carlos Resurreccion <resurreccion.cmc@gmail.com>
2024-07-15 13:16:22 +0800

    optimized line spacing

--- data-cleaning/rvs-functions.R
+++ data-cleaning/rvs-functions.R
@@ -44,30 +44,21 @@ print_summary_statistics <- function(rvss, rvs_icd9, rvs_map_list) {
   without_drg <- rvs_icd9[!rvs %in% names(rvs_map_list)]
   cat(
     sprintf('There are %d RVS codes without an ICD-9CM equivalent recognized by the TDRG ICD9CM\n', 
-            length(unique(without_drg$rvs)
-                   )
-            )
-    )
+            length(unique(without_drg$rvs))))
   
   mappable_rvs <- intersect(rvss, rvs_icd9$rvs)
   cat(sprintf('Of these, %d (%.2f%%) have a mapping to an ICD-9-CM code.\n', 
-              length(mappable_rvs), length(mappable_rvs) * 100 / length(rvss)
-              )
-      )
+              length(mappable_rvs), length(mappable_rvs) * 100 / length(rvss)))
   
   multi_mapped_rvs <- intersect(rvss, names(rvs_map_list))
   cat(
     sprintf('Of these, there are %d (%.2f%%) with more than one ICD9 equivalent recognized by the Thai ICD9 library.\n', 
             length(multi_mapped_rvs), 
-            length(multi_mapped_rvs) * 100 / length(mappable_rvs)
-            )
-    )
+            length(multi_mapped_rvs) * 100 / length(mappable_rvs)))
   
   unmappable_rvs <- setdiff(rvss, mappable_rvs)
   cat(sprintf('There are %d (%.2f%%) with no ICD-9-CM equivalents.\n', 
-              length(unmappable_rvs), length(unmappable_rvs) * 100 / length(rvss)
-              )
-      )
+              length(unmappable_rvs), length(unmappable_rvs) * 100 / length(rvss)))
 }
 
 # Main function to process RVS code mappings
