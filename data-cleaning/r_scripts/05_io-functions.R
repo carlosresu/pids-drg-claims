1fd9a17497f879d0daa8789c99ebb646623d04f0
Carlos Resurreccion <resurreccion.cmc@gmail.com>
2024-07-30 23:41:27 +0800

    edit typos

--- data-cleaning/r_scripts/05_io-functions.R
+++ data-cleaning/r_scripts/05_io-functions.R
@@ -19,7 +19,7 @@ split_and_save_parts <- function() {
           end_row <- min(part * rows_per_part, total_rows)
           dt <- full_data[start_row:end_row]
           setnames(dt, colnames(header))
-          if (to_debug) print(head(dt),2) # debug
+          if (to_debug) print(head(dt), 2) # debug
           fwrite(dt, chunk_file, quote = TRUE)
           rm(dt)
           gc()
@@ -103,16 +103,16 @@ read_appropriate_file <- function(part, to_sample) {
   #' @param part integer. The part number to process.
   #' @param to_sample logical. Whether to read the sample file or the full partial file.
   #' @return data.table. The processed data table.
-  
+
   chunk_file <- if (to_sample) {
     sampled_claims_file(part)
   } else {
     full_claims_file(part)
   }
-  
+
   dt <- fread(chunk_file, na.strings = na_values, colClasses = "character", header = TRUE)
 
-  if (to_debug) print(head(dt),2) # debug
+  if (to_debug) print(head(dt), 2) # debug
 
   # Drop columns
   if (any(drop_cols %in% colnames(dt))) {
@@ -122,13 +122,13 @@ read_appropriate_file <- function(part, to_sample) {
   replace_result <- replace_empty_with_na(dt, to_view_checks)
   dt <- replace_result$data
   replacement_summary <- replace_result$replacement_summary
-  
-  if (to_debug) print(head(dt),2) # debug
+
+  if (to_debug) print(head(dt), 2) # debug
 
   # Cast column types with checks
   for (col in names(col_classes)) {
     original_values <- dt[[col]]
-    
+
     dt[[col]] <- switch(col_classes[[col]],
       "character" = as.character(dt[[col]]),
       "factor" = {
@@ -143,15 +143,17 @@ read_appropriate_file <- function(part, to_sample) {
       },
       dt[[col]]
     )
-    
+
     # Check for NA coercion
     coerced_to_na <- which(is.na(dt[[col]]) & !is.na(original_values))
     if (length(coerced_to_na) > 0) {
-      cat(sprintf("Column '%s' coerced %d values to NA. First few original values: %s\n",
-                  col, length(coerced_to_na), paste(original_values[coerced_to_na][1:5], collapse = ", ")))
+      cat(sprintf(
+        "Column '%s' coerced %d values to NA. First few original values: %s\n",
+        col, length(coerced_to_na), paste(original_values[coerced_to_na][1:5], collapse = ", ")
+      ))
     }
   }
-  
+
   return(list(dt = dt, replacement_summary = replacement_summary))
 }
 
@@ -235,4 +237,3 @@ write_intermediate_file <- function(to_write, part, dt) {
     fwrite(dt, intermediate_file(part, fileext = TRUE), quote = TRUE)
   }
 }
-
