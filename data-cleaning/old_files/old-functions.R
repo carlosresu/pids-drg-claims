797da6de76331ca9874c45dc6793c33e07c839f6
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2024-10-22 12:28:13 +0000

    latest

--- data-cleaning/old_files/old-functions.R
+++ data-cleaning/old_files/old-functions.R
@@ -2973,3 +2973,59 @@
 #     by = .(get(paste0(col_name, "_orig")), get(col_name))
 #   ]
 # }
+# # Helper function to clean and compare clinical columns
+# clean_clinical_column <- function(dt, col_name) {
+#   # Store the original column
+#   dt[, (paste0(col_name, "_orig")) := dt[[col_name]]]
+
+#   # Apply the clean_column function,
+#   # which now returns a list of cleaned_col and is_covid
+#   cleaned_data <- clean_column(dt[[col_name]])
+
+#   # Extract cleaned column and is_covid flag
+#   cleaned_col <- cleaned_data$cleaned_col
+#   is_covid_flag <- cleaned_data$is_covid
+
+#   # Update the cleaned column
+#   dt[, (col_name) := cleaned_col]
+
+#   # Store the original column as a string (for comparison purposes)
+#   dt[, (paste0(col_name, "_orig")) := sapply(
+#     get(paste0(col_name, "_orig")), toString
+#   )]
+
+#   # Convert the cleaned column to a string for comparison
+#   dt[, (col_name) := sapply(get(col_name), toString)]
+
+#   # Convert is_covid to logical (if it's currently a factor)
+#   dt[, is_covid := as.logical(as.character(is_covid))]
+
+#   # If is_covid is FALSE and is_covid_flag is TRUE, set is_covid to TRUE
+#   dt[, is_covid := ifelse(
+#     is_covid == FALSE & is_covid_flag == TRUE, TRUE, is_covid
+#   )]
+
+#   # Ensure is_covid is logical and replace any NA with FALSE
+#   dt[is.na(is_covid), is_covid := FALSE]
+
+#   # Compare cleaning results
+#   # (only rows where the cleaned column differs from the original)
+#   comparison <- dt[
+#     !is.na(
+#       get(paste0(
+#         col_name,
+#         "_orig"
+#       ))
+#     ) & get(col_name) != get(paste0(
+#       col_name,
+#       "_orig"
+#     )),
+#     .(
+#       old_code = get(paste0(col_name, "_orig")),
+#       new_code = get(col_name), count = .N
+#     ),
+#     by = .(get(paste0(col_name, "_orig")), get(col_name))
+#   ]
+
+#   return(comparison)
+# }
---
+++ data-cleaning/r_scripts_v2/01.12.__.concatenate_r_files.R
@@ -0,0 +1,31 @@
+concatenate_r_files <- function(input_path, output_file) {
+  #' @title Concatenate R Files
+  #'
+  #' @description This function concatenates all .R files in
+  #' a specified directory into a single output file.
+  #'
+  #' @param input_path character. The directory containing the
+  #' .R files to concatenate.
+  #' @param output_file character. The path to the output file
+  #' where the concatenated content will be written.
+  #'
+  #' @return NULL.
+
+  # List all .R files in the directory
+  r_files <- list.files(
+    path = input_path,
+    pattern = "\\.R$", full.names = TRUE
+  )
+
+  # Delete the existing output file if it exists
+  if (file.exists(output_file)) {
+    file.remove(output_file)
+  }
+
+  # Read and concatenate contents
+  file_contents <- lapply(r_files, readLines)
+  concatenated_content <- unlist(file_contents)
+
+  # Write concatenated content to the output file
+  cat(concatenated_content, file = output_file, sep = "\n")
+}
