a396429bb7db35dcb774943cda26675586166a33
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2024-11-11 17:20:29 +0000

    latest

--- data-cleaning/drg-cleaning-all-years.r
+++ data-cleaning/drg-cleaning-all-years.r
@@ -17,7 +17,7 @@ run_notebook <- function(to_parallel) {
 }
 
 # Loop over the years (2019, 2022) with retry logic
-for (year in c(2018:2021)) {
+for (year in c(2018:2023)) {
   write(as.character(year), paths$year_to_load)
 
   if (!run_notebook(TRUE)) {
