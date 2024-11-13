b6b3161071fd09ec1bcdb6f1eb47699abe90ca4c
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2024-11-13 05:30:50 +0000

    latest

--- data-cleaning/r_scripts_v2/3.0.create_sample_files.R
+++ data-cleaning/r_scripts_v2/3.0.create_sample_files.R
@@ -1,5 +1,5 @@
-create_sample_files <- function(sample_part) {
-  partial_file_for_sampling <- here(raw_claims_parts_path, paste0(
+create_sample_files <- function(sample_part, sampled_claims_file) {
+  partial_file_for_sampling <- here::here(raw_claims_parts_path, paste0(
     full_claims_prefix, year_to_load,
     "_part_", sprintf("%02d", sample_part), "_of_", split_parts, ".rds"
   ))
