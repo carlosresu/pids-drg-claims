5b9cff3a97076644c6ffe57fb349b73cb6a77972
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2024-11-13 13:16:01 +0800

    latest

--- data-cleaning/r_scripts_v2/0.1.0.params_fpaths.R
+++ data-cleaning/r_scripts_v2/0.1.0.params_fpaths.R
@@ -33,7 +33,7 @@ split_parts <- 15
 # Sample size divisor: Formula for sample size is
 # (total_rows ÷ split_parts) ÷ sample_size_divisor.
 # Choose between 5, 25, 125, and 625
-sample_size_divisor <- 625
+sample_size_divisor <- sample_size_divisor
 
 # Columns to drop
 drop_cols <- c( # Which columns to drop
