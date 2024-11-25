29d785c155b30fd33574dcdd8770b916d9c09cf7
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2024-11-25 08:47:03 +0000

    latest

---
+++ data-cleaning/00a-parameters.r
@@ -0,0 +1,33 @@
+thread_offset <- 0
+
+sample_size_divisor <- 125
+
+# Whether to sample each split_part by sample_size_divisor
+# (useful when iterating through code runs in quick succession)
+to_sample <- TRUE
+# TODO: Add description here
+to_write <- TRUE
+# TODO: Add description here
+to_flush <- FALSE
+# TODO: Add description here
+to_parallel <- TRUE
+cat("Parallelization:", to_parallel, "\n")
+# TODO: Add description here
+to_debug <- FALSE
+verbose_output <- if (to_debug) TRUE else FALSE
+
+to_generate_subset <- TRUE
+
+to_py_prompt <- TRUE
+to_python <- TRUE
+to_generate_py_fwrite <- TRUE
+to_generate_feather <- TRUE
+to_py_bq <- FALSE
+
+to_thai_prompt <- TRUE
+to_thai <- TRUE
+to_thai_bq <- FALSE
+to_generate_thai_txt <- TRUE
+to_thai_all_years <- FALSE
+
+to_spc <- FALSE
