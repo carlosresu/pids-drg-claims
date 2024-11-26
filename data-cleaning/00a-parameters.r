c54192ceb4319d2dd773e571cbc0198afd995364
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2024-11-26 12:19:57 +0000

    trial run 2018 ipynb

--- data-cleaning/00a-parameters.r
+++ data-cleaning/00a-parameters.r
@@ -4,15 +4,15 @@ sample_size_divisor <- 25
 
 # Whether to sample each split_part by sample_size_divisor
 # (useful when iterating through code runs in quick succession)
-to_sample <- TRUE
+to_sample <- FALSE
 # TODO: Add description here
 to_write <- TRUE
 # TODO: Add description here
 to_flush <- FALSE
 # TODO: Add description here
-to_bq <- FALSE
+to_bq <- TRUE
 # TODO: Add description here
-to_post_cleaning_checks <- FALSE
+to_post_cleaning_checks <- TRUE
 # TODO: Add description here
 to_parallel <- TRUE
 cat("Parallelization:", to_parallel, "\n")
@@ -26,11 +26,11 @@ to_py_prompt <- TRUE
 to_python <- TRUE
 to_generate_py_fwrite <- TRUE
 to_generate_feather <- TRUE
-to_py_bq <- TRUE
+to_py_bq <- FALSE
 
 to_thai_prompt <- TRUE
 to_thai <- TRUE
-to_thai_bq <- TRUE
+to_thai_bq <- FALSE
 to_generate_thai_txt <- TRUE
 to_thai_all_years <- FALSE
 
