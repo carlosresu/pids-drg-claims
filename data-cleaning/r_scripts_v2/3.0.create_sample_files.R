78f54fde45a2a8cb5060417d01bf33451d9fd73a
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2025-01-20 06:26:34 +0000

    update seed setting due to random pdx generation

--- data-cleaning/r_scripts_v2/3.0.create_sample_files.R
+++ data-cleaning/r_scripts_v2/3.0.create_sample_files.R
@@ -1,9 +1,10 @@
-create_sample_files <- function(sample_part, sampled_claims_file) {
+create_sample_files <- function(sample_part, sampled_claims_file, seed = global_seed) {
   partial_file_for_sampling <- here::here(raw_claims_parts_path, paste0(
     full_claims_prefix, year_to_load,
     "_part_", sprintf("%02d", sample_part), "_of_", split_parts, ".rds"
   ))
   dt <- readRDS(partial_file_for_sampling)
+  set.seed(seed)
   dt <- dt[sample(.N, min(sample_size, .N))]
   saveRDS(dt, sampled_claims_file, compress = TRUE)
 }
