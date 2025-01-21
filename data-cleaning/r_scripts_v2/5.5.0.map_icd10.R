64b07b67f277f7568c8af69f7f740e780775ab69
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2025-01-22 07:46:35 +0800

    changed is.null to is.na in map_icd10 application of the function

--- data-cleaning/r_scripts_v2/5.5.0.map_icd10.R
+++ data-cleaning/r_scripts_v2/5.5.0.map_icd10.R
@@ -103,7 +103,7 @@ map_icd10 <- function(col) {
   # Apply mappings
   col_mapped <- lapply(col, function(codes) {
     unname(sapply(codes, function(code) {
-      if (!is.null(icd_mapping[[code]])) {
+      if (!is.na(icd_mapping[[code]])) {
         icd_mapping[[code]]
       } else {
         code
