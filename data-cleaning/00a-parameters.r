b7513761d3e53bd61d7173ceee4decbedbb58175
Carlos Miguel Resurreccion <resurreccion.cmc@gmail.com>
2025-02-27 08:27:10 +0000

    finalize run parameters

--- data-cleaning/00a-parameters.r
+++ data-cleaning/00a-parameters.r
@@ -1,13 +1,12 @@
 # Global parameters
 thread_offset <- 0
-
 sample_size_divisor <- 625
 
 # Whether to use single, standardized *_raw_master_*.rds
 to_create_std <- FALSE
 # Whether to sample each split_part by sample_size_divisor
 # (useful when iterating through code runs in quick succession)
-to_sample <- TRUE
+to_sample <- FALSE
 # TODO: Add description here
 to_write <- TRUE
 # To flush intermediate files
@@ -16,27 +15,27 @@ to_filter <- FALSE
 # clin_outpatient = FALSE, and id_hci = inst_level %in% "L1", "L2", "L3", "INF"
 to_flush <- FALSE
 # To push to BQ
-to_bq <- FALSE
+to_bq <- TRUE
 # To parallelize using unix-style multicore lapply
 to_parallel <- TRUE
 cat("Parallelization:", to_parallel, "\n")
 # TODO: Add description here
-to_debug <- FALSE
+to_debug <- if (!to_parallel) TRUE else FALSE
 verbose_output <- if (to_debug) TRUE else FALSE
 
 
 # Grouping parameters
-to_generate_subset <- TRUE
+to_generate_subset <- FALSE
 
-to_py_prompt <- TRUE
-to_python <- TRUE
-to_generate_py_fwrite <- TRUE
-to_generate_feather <- TRUE
+to_py_prompt <- FALSE
+to_python <- FALSE
+to_generate_py_fwrite <- FALSE
+to_generate_feather <- FALSE
 to_py_bq <- FALSE
 
-to_thai_prompt <- TRUE
-to_thai <- TRUE
-to_thai_bq <- TRUE
+to_thai_prompt <- FALSE
+to_thai <- FALSE
+to_thai_bq <- FALSE
 to_generate_thai_txt <- FALSE
 to_thai_all_years <- FALSE
 
